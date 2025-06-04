b4292b73097c0a52da714348bfca71f0282346ff
jaron.voided <jaron.voided@gmail.com>
2025-06-04 17:40:18 -0400

    Might have figured out Zee Recursion

--- ZerefConsoleApp/Program.cs
+++ ZerefConsoleApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using ZerefLibrary.ZCollections;
+
+internal class Program
+{
+    static void Main()
+    { 
+        var tree = new ZBinaryTree<int>();
+
+        tree.SetRoot(10);
+
+        var root = tree.Root;
+
+        tree.InsertLeft(root, 5);
+        tree.InsertRight(root, 15);
+
+        var node5 = root.Left;
+        var node15 = root.Right;
+
+        tree.InsertLeft(node5, 3);
+        tree.InsertRight(node5, 7);
+
+        tree.InsertRight(node15, 20);
+
+         foreach (var item in tree.TraverseInOrder())
+             Console.WriteLine(item);
+         
+         foreach (var item in tree.TraversePreOrder())
+             Console.WriteLine(item);
+         
+         foreach (var item in tree.TraversePostOrder())
+             Console.WriteLine(item);
+    }
+}
+
--- ZerefConsoleApp/obj/Debug/net9.0/ZerefConsoleApp.AssemblyInfo.cs
+++ ZerefConsoleApp/obj/Debug/net9.0/ZerefConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZerefConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e97559de82d21c3d822bc0708bd69a92bb0f5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a53754c8b1868d236430c6c892849800fb427f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZerefConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZerefConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
