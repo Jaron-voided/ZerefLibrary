d6b1199f937ad77986479c1dfc4adc3acc5bdb73
jaron.voided <jaron.voided@gmail.com>
2025-06-19 13:20:00 -0400

    Thought I pushed this already

--- ZerefConsoleApp/obj/Debug/net9.0/ZerefConsoleApp.AssemblyInfo.cs
+++ ZerefConsoleApp/obj/Debug/net9.0/ZerefConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZerefConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee097fd4dfed698bc8e3d9e119eae6b885501d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685f787be63edd96d54d3c026db2c17237ec1e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZerefConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZerefConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
